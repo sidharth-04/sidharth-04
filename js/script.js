be79332a24aeb1922d0d33c72b3f5b3af7d687ce
Sidharth Roy <68526404+sidharth-04@users.noreply.github.com>
2021-05-06 14:54:59 +0530

    Enhanced Scroll Animations
    Scroll animations enhanced for mobile users.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 const SECTIONS = {
-  skills: 4,
-  CSSoch: 1,
-  DoorStepSchool: 1,
-  Gurushiksha: 1,
-  Hackathons: 1,
-  CSSochGames: 14,
+  skills: 2,
+  CSSoch: 2,
+  DoorStepSchool: 2,
+  Gurushiksha: 2,
+  Hackathons: 2,
+  CSSochGames: 10,
   GamesCreative: 1,
   GamesEducational: 1,
   GamesBlockBased: 1,
   BottomContainer: 1,
-  GeneticAlgorithm: 6
+  GeneticAlgorithm: 3
 };
 let completedAnimating = [];
 
 $(document).ready(() => {
+  // Smooth Scrolling
   $('a[href*=\\#]:not([href=\\#])').on('click', function() {
     let target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
     if (target.length) {
-        $('html,body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-        return false;
+      $('html,body').animate({scrollTop: target.offset().top},1000);
+      return false;
     }
   });
 });
 
+// Scroll Animations
 $(window).scroll(function() {
   let windowBottom = $(this).scrollTop() + $(this).innerHeight();
   for (let section in SECTIONS) {
@@ -34,10 +34,9 @@ $(window).scroll(function() {
 
     let element = $("#"+section)
     let objectBottom = element.offset().top + element.outerHeight();
-    if (objectBottom < windowBottom + SECTIONS[section] * 10/100 * $(window).innerHeight()) {
+    if (objectBottom < windowBottom + SECTIONS[section] * 10/100 * 1000) {
       element.addClass("animate");
       completedAnimating.push(section);
     }
-
   }
 }).scroll();
